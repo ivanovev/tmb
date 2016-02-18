3bcfaf4a6cf50f50cd32ff56ecd50729bf9709b7
iev <iev@land.ru>
2016-02-18 16:59:32 +0300

    added starttmb.py

---
+++ starttmb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+#sys.path.append('/usr/lib/python3/dist-packages/starttmb')
+import util.startup
+import tmb
+
+def main():
+    util.startup.startup(tmb)
+
+if __name__ == '__main__':
+    main()
+
